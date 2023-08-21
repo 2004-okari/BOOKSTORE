3cb0659ec5eed71a4c8d4c7b743500350e6a5a57
okari <nyandika15@gmail.com>
2023-08-21 21:41:24 +0300

    add books

--- src/components/CreateBook.js
+++ src/components/CreateBook.js
@@ -1,10 +1,38 @@
+// CreateBook.js
+
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from './redux/booksSlice';
+
 function CreateBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const handleAddBook = () => {
+    dispatch(addBook({ id: Date.now(), title, author }));
+    setTitle('');
+    setAuthor('');
+  };
+
   return (
     <>
       <form>
-        <input type="text" placeholder="Book Title" />
-        <input type="text" placeholder="Author" />
-        <button type="button">Add Book</button>
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          value={author}
+          onChange={e => setAuthor(e.target.value)}
+        />
+        <button type="button" onClick={handleAddBook}>
+          Add Book
+        </button>
       </form>
     </>
   );
