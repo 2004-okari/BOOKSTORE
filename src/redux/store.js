0f58a4b86e781c7d44e27dfc2c75fc02ca628283
okari <nyandika15@gmail.com>
2023-08-21 19:46:12 +0300

    Add file structure

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/booksSlice';
+
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
+});
+
+export default store;
